b4c42b42e503fab9c546d2a79f0d8417549d091e
mmoran314 <mmoran9@ewu.edu>
2022-02-16 11:39:56 -0800

    changed name

--- composition.java
+++ composition.java
@@ -1,26 +1,26 @@
 // This file will be our composite with a list of all our foods
 import java.util.*;
 public class composition implements Food {
-    private ArrayList<Food> myMenu = new ArrayList<>();
+    private ArrayList<Food> myList = new ArrayList<>();
 
     @Override
     public void showFoodDetails() {
         int i = 0;
         for (Food myFood :
-                myMenu) {
+                myList) {
             System.out.print(i + ": ");
             myFood.showFoodDetails();
             i++;
         }
     }
     public void addFood(Food myFood) {
-        myMenu.add(myFood);
+        myList.add(myFood);
     }
     public void removeFood(Food myFood) {
-        myMenu.remove(myFood);
+        myList.remove(myFood);
     }
     public Food getFood(int i) {
-        return myMenu.get(i);
+        return myList.get(i);
     }
 }
 
